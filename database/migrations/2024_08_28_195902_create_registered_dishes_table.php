f7ea858a2c8bad6041099b374c0b23116659815e
pausr23 <paulina.serrano@ucr.ac.cr>
2024-09-06 12:27:18 -0600

    Prueba

---
+++ database/migrations/2024_08_28_195902_create_registered_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registered_dishes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dishes_categories_id')->constrained();
+            $table->string('title');
+            $table->string('description');
+            $table->string('image');
+            $table->decimal('dish_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registered_dishes');
+    }
+};
---
+++ database/migrations/2024_09_01_042855_create_transaction_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dishes_categories_id')->constrained();
+            $table->foreignId('registered_dishes_id')->constrained();
+            $table->decimal('registered_dishes_price');
+            $table->decimal('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
