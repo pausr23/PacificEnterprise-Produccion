5fdb3fa32368973eaf3513947e61580237e11815
mari13castro <mariajesus130402@gmail.com>
2024-10-27 01:28:03 -0600

    Clean Code del controler de dashboard, ajustes en el sidebar mobile y ajustes en el responsive de página principal

--- resources/views/factures/ordering.blade.php
+++ resources/views/factures/ordering.blade.php
@@ -6,10 +6,9 @@
 
     <div class="mr-5">
 
+    <div class="md:mr-5 hidden lg:block">
         <img class="lg:w-60 sm:w-32 sm:ml-0" src="https://i.ibb.co/KX69vv5/Pacific-Enterprise.png"
             alt="Pacific-Enterprise">
-
-
         <div id="sidebar-menu" class="hidden lg:grid pl-2 pt-6 text-white font-light text-sm font-main">
             <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
                 href="{{ route('dashboard.principal') }}">Panel Principal</a>
@@ -40,87 +39,66 @@
             <a class="py-3 mb-6 pl-4 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
                 href="{{ route('events.index') }}">Eventos</a>
         </div>
-
-
-        <div id="mobile-sidebar-menu"
-            class="absolute top-0  left-0 w-full h-screen bg-[#16161A] transform translate-x-full transition-transform duration-300 lg:hidden">
-            <div class="pl-2 pt-6 text-white font-light text-sm font-main">
-                <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                    href="{{ route('dashboard.principal') }}">Panel Principal</a>
-                <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg secondary-color transition-colors duration-300"
-                    href="{{ route('factures.ordering') }}">Punto de Venta</a>
-                <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                    href="{{ route('factures.order') }}">Órdenes</a>
-                <a class="py-3 mb-6 pl-2 block rounded-lg hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035]"
-                    href="{{ route('factures.history') }}">Historial de Ventas</a>
-
-                @if(Auth::check() && Auth::user()->job_titles_id == 1)
-                    <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                        href="{{ route('dishes.index') }}">Productos</a>
-                    <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                        href="{{ route('dishes.inventory') }}">Inventario</a>
-                    <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                        href="{{ route('suppliers.index') }}">Proveedores</a>
-                    <a class="py-3 mb-6 pl-4 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
-                        href="{{ route('events.index') }}">Eventos</a>
-                @endif
-
-                <a href="{{ route('admin.profile') }}" class="flex items-center cursor-pointer lg:m-2 sm:ml-0">
-                    <img class="lg:w-16 lg:h-16 sm:w-10 sm:h-10"
-                        src="https://img.icons8.com/?size=100&id=492ILERveW8G&format=png&color=000000" alt="">
-                    <div class="lg:ml-2">
-                        <p class="text-base font-semibold ml-1">{{ auth()->user()->name }}</p>
-                        <p class="text-sm">@ {{ auth()->user()->username }}</p>
-                    </div>
-                </a>
-            </div>
         </div>
 
 
-        <label
-            class="xxs:py-4 xxs:left-6 xxs:z-40 xxs:h-8 xxs:cursor-pointer lg:hidden xxs:flex md:items-center md:justify-center lg:none"
-            for="mobile-checkbox">
-            <input class="hidden" type="checkbox" id="mobile-checkbox" onClick="toggleMenu(this)" />
-            <svg xmlns="http://www.w3.org/2000/svg" class="xxs:h-6 xxs:w-6 lg:h-0 lg:w-0 md:h-8 md:w-8 ml-5 text-white "
-                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-        </label>
+        <div class="md:mr-5 lg:hidden  py-4 z-40 h-8 cursor-pointer flex items-center justify-start" 
+            onclick="toggleMenu()">
+            <img class="w-32" src="https://i.ibb.co/KX69vv5/Pacific-Enterprise.png" alt="Pacific-Enterprise Logo">
+        </div>
 
-        <div id="content" class="lg:ml-6">
+        <div id="mobile-sidebar-menu"
+            class="absolute top-0 left-0 w-full h-screen  transform -translate-x-full transition-transform duration-300 lg:hidden z-10 flex">
 
+            <div class="w-1/2 h-full bg-[#16161A]">
+                <div class="mt-10 pl-2 flex items-center justify-start py-4 z-40 h-8 cursor-pointer" 
+                    onclick="toggleMenu()">
+                    <img class="w-32 " src="https://i.ibb.co/KX69vv5/Pacific-Enterprise.png" alt="Pacific-Enterprise Logo">
+                </div>
+                <div class="pl-2 pt-6 text-white font-light text-sm font-main">
+                    <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
+                        href="{{ route('dashboard.principal') }}">Panel Principal</a>
+                    <a class="py-3 mb-6 pl-2 secondary-color transition-colors duration-300 hover:bg-[#323035] focus:bg-[#323035]  block rounded-lg"
+                        href="{{ route('factures.ordering') }}">Punto de Venta</a>
+                    <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
+                        href="{{ route('factures.order') }}">Órdenes</a>
+                    <a class="py-3 mb-6 pl-2 block rounded-lg hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035]"
+                        href="{{ route('factures.history') }}">Historial de Ventas</a>
+
+                    @if(Auth::check() && Auth::user()->job_titles_id == 1)
+                        <a class="py-3 mb-6 pl-2 block rounded-lg hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035]"
+                        href="{{ route('dishes.index') }}">Productos</a>
+                        <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
+                        href="{{ route('dishes.inventory') }}">Inventario</a>
+                        <a class="py-3 mb-6 pl-2 hover:bg-[#323035] focus:bg-[#323035] active:bg-[#323035] block rounded-lg"
+                        href="{{ route('suppliers.index') }}">Proveedores</a>
+                    @endif
+
+                    <a href="{{ route('admin.profile') }}" class="flex items-center cursor-pointer lg:m-2 sm:ml-0">
+                        <img class="lg:w-16 lg:h-16 xxs:w-16 sm:w-10 sm:h-10"
+                            src="https://img.icons8.com/?size=100&id=492ILERveW8G&format=png&color=000000" alt="">
+                        <div class="lg:ml-2">
+                            <p class="text-base font-semibold ml-1">{{ auth()->user()->name }}</p>
+                            <p class="text-sm">@ {{ auth()->user()->username }}</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="w-1/2 h-full bg-black opacity-50"></div>
         </div>
 
         <script>
-            function toggleMenu(checkbox) {
+            function toggleMenu() {
                 const mobileMenu = document.getElementById('mobile-sidebar-menu');
-                const content = document.getElementById('content');
-                const isChecked = checkbox.checked;
 
-
-                if (isChecked) {
-                    mobileMenu.style.transform = 'translateX(0)';
-                    mobileMenu.style.display = 'block';
+                if (mobileMenu.style.transform === 'translateX(0%)') {
+                    mobileMenu.style.transform = 'translateX(-100%)';
                 } else {
-                    mobileMenu.style.transform = 'translateX(100%)';
-                    mobileMenu.style.display = 'none';
+                    mobileMenu.style.transform = 'translateX(0%)';
                 }
-
-
-                content.style.display = isChecked ? 'none' : 'block';
-
-
-                const hamburgerIcon = document.querySelector('label[for="mobile-checkbox"]');
-                hamburgerIcon.style.display = isChecked ? 'none' : 'flex';
             }
-
-            window.onload = function () {
-                const checkbox = document.getElementById('mobile-checkbox');
-                checkbox.checked = false;
-                toggleMenu(checkbox);
-            };
         </script>
-
     </div>
 
     <div class="grid lg:grid-cols-[65%,30%] xxs:grid-cols-1">
